2d679fac309d00bc27ece76a74c175b6dd635a39
hyeonQyu <dhk0561@naver.com>
2020-03-16 11:33:00 +0900

    Controller 객체 구현 [@RequestMapping를 이용한 url 맵핑, 요청 파라미터 및 커맨드 객체 이용]

--- memberManagement/src/main/java/com/iowe/member1/controller/MemberController.java
+++ memberManagement/src/main/java/com/iowe/member1/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.iowe.member1.Member;
 import com.iowe.member1.service.MemberService;
@@ -15,6 +16,7 @@ import com.iowe.member1.service.MemberService;
 
 
 @Controller
+@RequestMapping("/member")	// /member + 각 메소드 속성 값
 public class MemberController {
 
 //	MemberService service = new MemberService();
@@ -22,30 +24,34 @@ public class MemberController {
 	@Resource(name="memService")
 	MemberService service;
 	
+	// method가 GET방식이면 명시해주지 않아도 됨, value만 있을 경우 value=도 생략 가능
 	@RequestMapping(value="/memJoin", method=RequestMethod.POST)
-	public String memJoin(Model model, HttpServletRequest request) {
-		String memId = request.getParameter("memId");
-		String memPw = request.getParameter("memPw");
-		String memMail = request.getParameter("memMail");
-		String memPhone1 = request.getParameter("memPhone1");
-		String memPhone2 = request.getParameter("memPhone2");
-		String memPhone3 = request.getParameter("memPhone3");
+	public String memJoin(Member member) {//Model model, HttpServletRequest request) {
+//		String memId = request.getParameter("memId");
+//		String memPw = request.getParameter("memPw");
+//		String memMail = request.getParameter("memMail");
+//		String memPhone1 = request.getParameter("memPhone1");
+//		String memPhone2 = request.getParameter("memPhone2");
+//		String memPhone3 = request.getParameter("memPhone3");
 		
-		service.memberRegister(memId, memPw, memMail, memPhone1, memPhone2, memPhone3);
+//		service.memberRegister(memId, memPw, memMail, memPhone1, memPhone2, memPhone3);
+		service.memberRegister(member.getMemId(), member.getMemPw(), member.getMemMail(), 
+					member.getMemPhone1(), member.getMemPhone2(), member.getMemPhone3());
 		
-		model.addAttribute("memId", memId);
-		model.addAttribute("memPw", memPw);
-		model.addAttribute("memMail", memMail);
-		model.addAttribute("memPhone", memPhone1 + " - " + memPhone2 + " - " + memPhone3);
+//		model.addAttribute("memId", memId);
+//		model.addAttribute("memPw", memPw);
+//		model.addAttribute("memMail", memMail);
+//		model.addAttribute("memPhone", memPhone1 + " - " + memPhone2 + " - " + memPhone3);
 		
 		return "memJoinOk";
 	}
 	
 	@RequestMapping(value="/memLogin", method=RequestMethod.POST)
-	public String memLogin(Model model, HttpServletRequest request) {
-		
-		String memId = request.getParameter("memId");
-		String memPw = request.getParameter("memPw");
+	public String memLogin(Model model, @RequestParam("memId") String memId, 
+			@RequestParam(value="memPw", required=false, defaultValue="1234") String memPw) {	//HttpServletRequest request) {
+										// required가 true이면 반드시 입력되어야 하는 값
+//		String memId = request.getParameter("memId");
+//		String memPw = request.getParameter("memPw");
 		
 		Member member = service.memberSearch(memId, memPw);
 		
