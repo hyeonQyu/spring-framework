37ddb06558a9669bc81d467fef79476b0f581afa
hyeonQyu <dhk0561@naver.com>
2020-08-08 21:13:37 +0900

    MVC 게시판 프로젝트 및 데이터베이스 테이블 생성

---
+++ spring_db.sql
@@ -0,0 +1,15 @@
+create table board(
+    bId number(4) primary key,
+    bName varchar2(20),
+    bTitle varchar2(100),
+    bContent varchar2(300),
+    bDate date default sysdate,
+    bHit number(4) default 0,
+    bGroup number(4),
+    bStep number(4),
+    bIndent number(4)
+);
+
+commit;
+
+create sequence board_seq;
